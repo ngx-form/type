c2267b4e598e337b94a68e81ad88d7bc241bede9
Diabeu <scibor.rudnicki@ster-project.pl>
2017-05-10 14:44:45 +0200

    refactor: add true or false

--- src/ngx-form-type.ts
+++ src/ngx-form-type.ts
@@ -1,3 +1,4 @@
+
 export type element = 'checkbox' | 'input' | 'select';
 export type event = 'cancelled' | 'changed' | 'submitted';
 
@@ -6,9 +7,10 @@ export type event = 'cancelled' | 'changed' | 'submitted';
   http://w3c.github.io/html-reference/elements.html
 */
 export type autocomplete = 'on' | 'off';
-export type autofocus = 'autofocus' | '';
-export type disabled = 'disabled' | '';
-export type input = 'date' | 'datetime' | 'datetime-local' | 'email' | 'month' | 'password' | 'range' | 'tel' | 'text' | 'time' | 'url' |
-  'week';
-export type readonly = 'readonly' | '';
-export type required = 'required' | '';
+export type autofocus = 'autofocus' | '' | true | false;
+export type disabled = 'disabled' | '' | true | false;
+export type checked = 'checked' | '' | true | false;
+export type input = 'button' | 'color' | 'checkbox' | 'date' | 'datetime' | 'datetime-local' | 'email' | 'file' | 'hidden' |
+  'image' | 'month' | 'number' | 'password' | 'range' | 'reset' | 'search' | 'submit' | 'tel' | 'text' | 'time' | 'url' | 'week';
+export type readonly = 'readonly' | '' | true | false;
+export type required = 'required' | '' | true | false;
