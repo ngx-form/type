1becd00ec80848c497af5e0ef270b9c5612e6dc9
Diabeu <scibor.rudnicki@ster-project.pl>
2017-06-22 13:43:18 +0200

    refactor(index): change import and export

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,69 @@
 import {
-  autocomplete,
-  autofocus,
-  disabled,
-  element,
-  event,
-  input,
-  readonly,
-  required
- } from './ngx-form-type';
+  acceptType, accept, altType, alt, accesskeyType, accesskey, autocompleteType, autocomplete, autofocusType, autofocus,
+
+  checkedType, checked, classType, contenteditableType, contenteditable, contextmenuType, contextmenu,
+
+  disabledType, disabled, dirType, dir, dirnameType, dirname, dropzoneType, dropzone,
+
+  element, event,
+
+  formType, form, formactionType, formaction, formenctypeType, formenctype, formmethodType, formmethod, formtargetType, formtarget,
+  formnovalidateType, formnovalidate,
+
+  heightType, height, hiddenType, hidden,
+
+  idType, id, input,
+
+  langType, lang, listType, list,
+
+  maxlengthType, maxlength, maxType, max, minType, min, multipleType, multiple,
+
+  nameType, name,
+
+  patternType, pattern, placeholderType, placeholder,
+
+  readonlyType, readonly, requiredType, required,
+
+  sizeType, size, srcType, src, spellcheckType, spellcheck, stepType, step, styleType, style,
+
+  tabindexType, tabindex, titleType, title, translateType, translate, typeType, type,
+
+  widthType, width,
+
+  valueType, value
+ } from './type';
 
 export {
-  autocomplete,
-  autofocus,
-  disabled,
-  element,
-  event,
-  input,
-  readonly,
-  required
-}
+  acceptType, accept, altType, alt, accesskeyType, accesskey, autocompleteType, autocomplete, autofocusType, autofocus,
+
+  checkedType, checked, classType, contenteditableType, contenteditable, contextmenuType, contextmenu,
+
+  disabledType, disabled, dirType, dir, dirnameType, dirname, dropzoneType, dropzone,
+
+  element, event,
+
+  formType, form, formactionType, formaction, formenctypeType, formenctype, formmethodType, formmethod, formtargetType, formtarget,
+  formnovalidateType, formnovalidate,
+
+  heightType, height, hiddenType, hidden,
+
+  idType, id, input,
+
+  langType, lang, listType, list,
+
+  maxlengthType, maxlength, maxType, max, minType, min, multipleType, multiple,
+
+  nameType, name,
+
+  patternType, pattern, placeholderType, placeholder,
+
+  readonlyType, readonly, requiredType, required,
+
+  sizeType, size, srcType, src, spellcheckType, spellcheck, stepType, step, styleType, style,
+
+  tabindexType, tabindex, titleType, title, translateType, translate, typeType, type,
+
+  widthType, width,
+
+  valueType, value
+};
